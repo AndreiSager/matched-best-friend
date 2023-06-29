f5a4ec07d7b06b88a8e60c9177c30800b5aeb27e
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-06-29 18:20:07 +0800

    Setup complete

---
+++ my-app/components/Footer.tsx
@@ -0,0 +1,65 @@
+import { footerLinks } from "@/constants"
+import Link from "next/link"
+import Image from "next/image"
+
+type ColumnProps = {
+    title: string;
+    links: Array<string>
+}
+
+const FooterColumn = ({title , links}: ColumnProps) => (
+    <div className="footer_column">
+        <h4 className="font-semibold">{title}</h4>
+        <ul className="flex flex-col gap-2 font-normal">
+            {links.map((link) => (
+                <Link href="/" key={link}>{link}</Link>
+            ))}
+        </ul>
+    </div>
+)
+
+const Footer = () => {
+    return(
+        <footer className="flexStart footer w-full">
+            <div className="flex flex-col gap-12 w-full">
+                <div className="flex items-start flex-col w-full">
+                    <Image
+                        src="/logo-purple.svg"
+                        height={200}
+                        width={150}
+                        alt="Doggo Match"
+                    />
+                    <p className="text-start text-sm font-normal mt-5 max-w-xs">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque 
+                        excepturi eiuss officiis, magnam non.
+                    </p>
+                </div>
+                <div className="flex flex-wrap gap-12">
+                    <FooterColumn title={footerLinks[0].title} links={footerLinks[0].links}/>
+                    <div className="flex flex-1 flex-col gap-4">
+                        <FooterColumn title={footerLinks[1].title} links={footerLinks[1].links}/>
+                        <FooterColumn title={footerLinks[2].title} links={footerLinks[2].links}/>
+                    </div>
+                    <FooterColumn title={footerLinks[3].title} links={footerLinks[3].links}/>
+                    <div className="flex flex-1 flex-col gap-4">
+                        <FooterColumn title={footerLinks[4].title} links={footerLinks[4].links}/>
+                        <FooterColumn title={footerLinks[5].title} links={footerLinks[5].links}/>
+                    </div>
+                    <FooterColumn title={footerLinks[6].title} links={footerLinks[6].links}/>
+
+                </div>
+            </div>
+            <div className="flexBetween footer_copyright">
+                <p>@ 2023 Doggo Match. All rights reserved</p>
+                <p className="text-gray">
+                    <span className="text-black font-semibold">
+                        10,214
+                    </span> projects
+                     submitted
+                </p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
